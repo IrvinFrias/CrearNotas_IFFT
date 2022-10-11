c217f6b2b20d4ea34ed1d7bc904600f0bab35aaa
Irvin Fernando Frias Torres <irvin.frias.unam@outlook.com>
2022-10-10 19:23:59 -0500

    Clase_04 completed

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//Immediately invoked function expression IIFE
+( () => {
+
 const btn = document.querySelector("[data-form-btn]");
 
 //Arrow funtion
@@ -5,18 +8,44 @@ const createTask = (evento) => {
     const input = document.querySelector("[data-form-input]");
     const value = input.value;
     const list = document.querySelector("[data-list]");
-    const task = document.createElement("li");
-    task.classList.add('card');
-    const content = `<div>
-                    <i class="far fa-check-square icon"></i>
-                    <span class="task">${value}</span>
-                </div>
+    const task = document.createElement("li"); //1. Crear Etiqueta
+    task.classList.add('card'); // Agrega clase a Etiqueta.
+
+    const taskContent = document.createElement("div");
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement("span");
+    titleTask.classList.add('task');
+    titleTask.innerText = value;
+    taskContent.appendChild(titleTask);
+
+    const content = `
+                    
+                
                 <i class="fas fa-trash-alt trashIcon icon"></i>`
-    task.innerHTML = content;
-    list.appendChild(task);
+    //task.innerHTML = content; //2. Asignar contenido a la etiqueta
+    task.appendChild(taskContent);
+    list.appendChild(task); //3. Agregar hijo a un etiqueta existente
     input.value = "";
     evento.preventDefault();
 }
 
 btn.addEventListener("click",createTask);
+
+
+
+const checkComplete = ()=>{
+    const i = document.createElement("i");
+    i.classList.add('far','fa-check-square','icon');
+    i.addEventListener("click", completeTask)
+    return i;
+}
+
+const completeTask = (event) => {
+    const element = event.target;
+    element.classList.toggle("fas");
+    element.classList.toggle('complteIcon');
+    element.classList.toggle('far');
+}
+
+})();
 
