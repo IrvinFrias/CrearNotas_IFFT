6d3f5b1691bc68f01713d7fff2ae1964fc8e7c14
Irvin Fernando Frias Torres <irvin.frias.unam@outlook.com>
2022-10-10 20:02:03 -0500

    Clase_05 completed

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-//Immediately invoked function expression IIFE
-( () => {
+//Immediately invoked function expression IIFE ( () => {})();
+
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
+
 
 const btn = document.querySelector("[data-form-btn]");
 
@@ -8,44 +11,21 @@ const createTask = (evento) => {
     const input = document.querySelector("[data-form-input]");
     const value = input.value;
     const list = document.querySelector("[data-list]");
-    const task = document.createElement("li"); //1. Crear Etiqueta
+    const task = document.createElement("li");
     task.classList.add('card'); // Agrega clase a Etiqueta.
-
     const taskContent = document.createElement("div");
-    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(checkComplete()); //icono check
     const titleTask = document.createElement("span");
     titleTask.classList.add('task');
     titleTask.innerText = value;
     taskContent.appendChild(titleTask);
-
-    const content = `
-                    
-                
-                <i class="fas fa-trash-alt trashIcon icon"></i>`
-    //task.innerHTML = content; //2. Asignar contenido a la etiqueta
     task.appendChild(taskContent);
-    list.appendChild(task); //3. Agregar hijo a un etiqueta existente
+    task.appendChild(deleteIcon());
+    list.appendChild(task);
     input.value = "";
     evento.preventDefault();
 }
 
 btn.addEventListener("click",createTask);
 
-
-
-const checkComplete = ()=>{
-    const i = document.createElement("i");
-    i.classList.add('far','fa-check-square','icon');
-    i.addEventListener("click", completeTask)
-    return i;
-}
-
-const completeTask = (event) => {
-    const element = event.target;
-    element.classList.toggle("fas");
-    element.classList.toggle('complteIcon');
-    element.classList.toggle('far');
-}
-
-})();
 
